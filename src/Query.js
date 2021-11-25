e748556686682da376f6d339b4dbf959fc2fdffc
Omer Zirh <omerzirh@gmail.com>
2021-11-25 16:27:25 +0100

    used search queries

--- src/Query.js
+++ src/Query.js
@@ -1,18 +1,21 @@
 const githubQuery = {
   query: `
-    {
-      viewer {
-        name
-        repositories(first: 10) {
-          nodes {
-            name
-            description
-            id
-            url
-          }
+  {
+    viewer {
+      name
+    }
+    search(query: "user:omerzirh sort:updated-desc", type: REPOSITORY, first: 10) {
+      nodes {
+        ... on Repository {
+          name
+          description
+          id
+          url
         }
       }
     }
+  }
+  
     
     `,
 };
